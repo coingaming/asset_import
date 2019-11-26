d4fa17679051d4e6ad907462392d158f23806048
Reio Piller <reio@heathmont.net>
2019-11-26 14:44:57 +0200

    AssetHook

--- index.js
+++ index.js
@@ -36,40 +36,63 @@ const AssetHook = {
     const headEl = document.getElementsByTagName("head")[0];
     const bodyEl = document.getElementsByTagName("body")[0];
 
-    this.el.loadingAssets = assets;
+    const assetFiles = assets.split(" ");
 
-    const onload = () => {
-      const index = this.el.loadingAssets.indexOf(item);
+    this.el.loadingAssets = assetFiles.slice(0);
+
+    const onload = (fileName) => {
+      const index = this.el.loadingAssets.indexOf(fileName);
       if (index !== -1) {
         this.el.loadingAssets.splice(index, 1);
         if (!this.el.loadingAssets.length) {
-          console.log("Loaded", this);
+          const hookFunc = this.__view.liveSocket.hooks[hook].mounted
+          if (hookFunc) {
+            hookFunc.apply(this);
+          }
         }
       }
     };
 
-    assets.split(" ").forEach(fileName => {
+    assetFiles.forEach(fileName => {
       if (fileName.substr(-4) === '.css') {
         if (headEl.querySelectorAll(`link[rel="stylesheet"][href="${fileName}"]`).length) {
+          onload(fileName);
           return;
         }
         const el = document.createElement("link");
         el.rel = "stylesheet";
         el.type = "text/css";
         el.href = fileName;
-        el.onload = onload;
+        el.onload = () => onload(fileName);
         headEl.appendChild(el)
       }
       else if (fileName.substr(-3) === '.js') {
         if (document.querySelectorAll(`script[src="${fileName}"]`).length) {
+          onload(fileName);
           return;
         }
         const el = document.createElement('script');;
         el.src = fileName;
-        el.onload = onload;
+        el.onload = () => onload(fileName);
         bodyEl.appendChild(el);
       }
     });
+  },
+
+  updated() {
+    const { hook } = this.el.dataset;
+    const hookFunc = this.__view.liveSocket.hooks[hook].updated
+    if (hookFunc) {
+      hookFunc.apply(this);
+    }
+  },
+
+  destroyed() {
+    const { hook } = this.el.dataset;
+    const hookFunc = this.__view.liveSocket.hooks[hook].destroyed
+    if (hookFunc) {
+      hookFunc.apply(this);
+    }
   }
 
 };
